2bc1b07b6645728beed15dfc832a15c1da162dc0
Juan Martínez Osorio <juan24.martinez05@gmail.com>
2019-05-09 20:16:20 -0500

    Primer commit

---
+++ api/app/app.php
@@ -0,0 +1,7 @@
+<?php
+  require $context_app . 'vendor/autoload.php';
+
+  // Agregar la configuración de la aplicación.
+  $settings = require $context_app . '/app/settings.php';
+
+?>
---
+++ api/app/settings.php
@@ -0,0 +1,19 @@
+<?php
+  // Confifuración  de la base de datos.
+  $db = require $context_app . 'app/database/config.php';
+
+  // key de encriptación
+  $jwt = array('key' => 'W&qp]hbzGN$F7(_J', 'algorithms' => array('HS256'));
+
+  // Configuracion de la App.
+  $settings = array(
+    'displayErrorDetails' => true,
+    'determineRouteBeforeAppMiddleware' => true,
+    'db' => $db,
+    'jwt' => $jwt
+  );
+
+  // se Agrega el contexto de al app.
+  $settings['context'] = $context_app;
+  return $settings;
+?>
